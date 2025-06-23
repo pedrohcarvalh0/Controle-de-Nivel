c3a85d7dffd22989d1b7971e5a187c4e8f1bffc2
saturnino028 <marcossaturnino028@gmail.com>
2025-06-23 11:51:47 -0300

    Antes de fazer o merge com a interface-de-usuario

--- src/ControleDeNivel.c
+++ src/ControleDeNivel.c
@@ -104,7 +104,6 @@ bool controle_bomba()
         controle_bomba = 1;
         gpio_put(ACI_BOMBA, controle_bomba);
         
-        desenho_pio_green(v, VALOR_LED, pio, sm, R, G, B);
         printf("Bomba Desligada\n");
         sleep_ms(1000);
     }
@@ -125,45 +124,3 @@ bool controle_bomba()
 
     return controle_bomba;
 }
-
-void set_matrix () {
-
-    // Setando matriz de leds
-   double r = 0.0, b = 0.0 , g = 0.0;
-   bool ok;
-   ok = set_sys_clock_khz(128000, false);
-   pio = pio0;
-
-   uint offset = pio_add_program(pio, &main_program);
-   uint sm = pio_claim_unused_sm(pio, true);
-   main_program_init(pio, sm, offset, OUT_PIN);
-}
-
-//rotina para definição da intensidade de cores do led
-uint32_t matrix_rgb(double b, double r, double g)
-{
-  //unsigned char R, G, B;
-  R = r * 255;
-  G = g * 255;
-  B = b * 255;
-  return (G << 24) | (R << 16) | (B << 8);
-}
-
-// Desenha na matriz de leds em verde
-void desenho_pio_green(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b){
-
-    for (int16_t i = 0; i < NUM_PIXELS; i++) {
-            valor_led = matrix_rgb(b = 0.0, r=0.0, desenho[24-i]);
-            pio_sm_put_blocking(pio, sm, valor_led);
-    }
-}
-
-// Desenha na matriz de leds em vermelho
-void desenho_pio_red(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b){
-
-    for (int16_t i = 0; i < NUM_PIXELS; i++) {
-            valor_led = matrix_rgb(b = 0.0, desenho[24-i], g = 0.0);
-            pio_sm_put_blocking(pio, sm, valor_led);
-    }
-}
-
