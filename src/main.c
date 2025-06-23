70f400e6a6953955e0d249c93276392f64488552
saturnino028 <marcossaturnino028@gmail.com>
2025-06-23 13:02:49 -0300

    Merge interface-de-usuario

--- src/main.c
+++ src/main.c
@@ -6,11 +6,31 @@ int main()
     ini_echo();
     init_bomba();
     set_matrix();
+
+    sleep_ms(5000);
     
-    while (true) 
+    // Inicializar Wi-Fi e servidor HTTP
+    init_wifi();
+    start_http_server();
+    
+    printf("Sistema inicializado com sucesso!\n");
+    printf("Acesse a interface web pelo IP mostrado acima\n");
+    
+    // Loop principal
+    while (true)
     {
+        // Processar requisições de rede
+        cyw43_arch_poll();
+        
+        // Executar controle da bomba
         controle_bomba();
+        
+        // Pequeno delay para não sobrecarregar o sistema
+        sleep_ms(100);
     }
+
+    // Cleanup (nunca será executado neste caso)
+    cyw43_arch_deinit();
 
     return 0;
 }
